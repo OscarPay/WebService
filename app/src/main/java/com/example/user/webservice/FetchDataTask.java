9eadac2440bb7ae973b7b69be87963a29a9a1811
Nikez44 <nike-o_94@hotmail.com>
2016-04-01 23:00:07 -0600

    Se actualizo para el proyecto del segunda parcial

--- app/src/main/java/com/example/user/webservice/FetchDataTask.java
+++ app/src/main/java/com/example/user/webservice/FetchDataTask.java
@@ -2,8 +2,13 @@ package com.example.user.webservice;
 
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.text.format.Time;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,32 +18,52 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by USER on 15/03/2016.
  */
-public class FetchDataTask extends AsyncTask<String, Void, String> {
+public class FetchDataTask extends AsyncTask<String, Void, String[]> {
 
-    private TextView label;
-    public static final String URL_TAG = "URL_TAG";
+    private ArrayAdapter<String> adapter;
+    public static final String LOG_TAG = FetchDataTask.class.getSimpleName();
 
-    public FetchDataTask(TextView label) {
-        this.label = label;
+    public FetchDataTask(ArrayAdapter<String> adapter) {
+        this.adapter = adapter;
     }
 
     @Override
-    protected String doInBackground(String... params) {
+    protected String[] doInBackground(String... params) {
 
         String result = "No hay Resultados";
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
 
+        String id = "3523349";
+        String format = "json";
+        String units = "metric";
+        int numDays = 14;
+        String apiKey = "72e59c6d3d400f867f8808af3cc2f9a3";
+
         try {
 
-            final String BASE_URL = "http://hmkcode.appspot.com/rest/controller/get.json";
-            Uri buildUri = Uri.parse(BASE_URL).buildUpon().build();
+            final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+            final String CITY_ID_PARAM = "id";
+            final String FORMAT_PARAM = "mode";
+            final String UNITS_PARAM = "units";
+            final String DAYS_PARAM = "cnt";
+            final String API_KEY_PARAM = "appid";
+
+            Uri buildUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                    .appendQueryParameter(CITY_ID_PARAM, id)
+                    .appendQueryParameter(FORMAT_PARAM, format)
+                    .appendQueryParameter(UNITS_PARAM, units)
+                    .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                    .appendQueryParameter(API_KEY_PARAM, apiKey)
+                    .build();
+
             URL url = new URL(buildUri.toString());
-            Log.d(URL_TAG, buildUri.toString());
+            Log.d(LOG_TAG, buildUri.toString());
 
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
@@ -82,17 +107,136 @@ public class FetchDataTask extends AsyncTask<String, Void, String> {
                 try {
                     reader.close();
                 } catch (final IOException e) {
-                    Log.e("URL_TAG", "Error closing stream", e);
+                    Log.e("LOG_TAG", "Error closing stream", e);
                 }
             }
         }
 
-        return result;
+        try {
+            return getWeatherDataFromJson(result, numDays);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-        label.setText(s);
+    protected void onPostExecute(String[] days) {
+        super.onPostExecute(days);
+        if(days != null) {
+            adapter.clear();
+            for (String dayForecastStr : days) {
+                adapter.add(dayForecastStr);
+            }
+            //New data is back from the server
+        }
+
+    }
+
+
+    /**
+     * Take the String representing the complete forecast in JSON Format and
+     * pull out the data we need to construct the Strings needed for the wireframes.
+     * <p/>
+     * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+     * into an Object hierarchy for us.
+     */
+    private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+            throws JSONException {
+
+        // These are the names of the JSON objects that need to be extracted.
+        final String OWM_LIST = "list";
+        final String OWM_WEATHER = "weather";
+        final String OWM_TEMPERATURE = "temp";
+        final String OWM_MAX = "max";
+        final String OWM_MIN = "min";
+        final String OWM_DESCRIPTION = "main";
+
+        JSONObject forecastJson = new JSONObject(forecastJsonStr);
+        JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+        // OWM returns daily forecasts based upon the local time of the city that is being
+        // asked for, which means that we need to know the GMT offset to translate this data
+        // properly.
+
+        // Since this data is also sent in-order and the first day is always the
+        // current day, we're going to take advantage of that to get a nice
+        // normalized UTC date for all of our weather.
+
+        Time dayTime = new Time();
+        dayTime.setToNow();
+
+        // we start at the day returned by local time. Otherwise this is a mess.
+        int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+        // now we work exclusively in UTC
+        dayTime = new Time();
+
+        String[] resultStrs = new String[numDays];
+        for (int i = 0; i < weatherArray.length(); i++) {
+            // For now, using the format "Day, description, hi/low"
+            String day;
+            String description;
+            String highAndLow;
+
+            // Get the JSON object representing the day
+            JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+            // The date/time is returned as a long.  We need to convert that
+            // into something human-readable, since most people won't read "1400356800" as
+            // "this saturday".
+            long dateTime;
+            // Cheating to convert this to UTC time, which is what we want anyhow
+            dateTime = dayTime.setJulianDay(julianStartDay + i);
+            day = getReadableDateString(dateTime);
+
+            // description is in a child array called "weather", which is 1 element long.
+            JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+            description = weatherObject.getString(OWM_DESCRIPTION);
+
+            // Temperatures are in a child object called "temp".  Try not to name variables
+            // "temp" when working with temperature.  It confuses everybody.
+            JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+            double high = temperatureObject.getDouble(OWM_MAX);
+            double low = temperatureObject.getDouble(OWM_MIN);
+
+            highAndLow = formatHighLows(high, low);
+            resultStrs[i] = day + " - " + description + " - " + highAndLow;
+        }
+
+        for (String s : resultStrs) {
+            Log.v(LOG_TAG, "Forecast entry: " + s);
+        }
+        return resultStrs;
+
+    }
+
+    /* The date/time conversion code is going to be moved outside the asynctask later,
+       * so for convenience we're breaking it out into its own method now.
+       */
+    private String getReadableDateString(long time) {
+        // Because the API returns a unix timestamp (measured in seconds),
+        // it must be converted to milliseconds in order to be converted to valid date.
+        SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+        return shortenedDateFormat.format(time);
+    }
+
+    /**
+     * Prepare the weather high/lows for presentation.
+     */
+    private String formatHighLows(double high, double low) {
+
+        high = (high * 1.8) + 32;
+        low = (low * 1.8) + 32;
+
+
+        // For presentation, assume the user doesn't care about tenths of a degree.
+        long roundedHigh = Math.round(high);
+        long roundedLow = Math.round(low);
+
+        String highLowStr = roundedHigh + "/" + roundedLow;
+        return highLowStr;
     }
 }
--- app/src/main/java/com/example/user/webservice/MainActivity.java
+++ app/src/main/java/com/example/user/webservice/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example.user.webservice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView label;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        label = (TextView) findViewById(R.id.label);
+        listView = (ListView) findViewById(R.id.list_view);
 
-        FetchDataTask fetchDataTask = new FetchDataTask(label);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        listView.setAdapter(adapter);
+
+        FetchDataTask fetchDataTask = new FetchDataTask(adapter);
         fetchDataTask.execute("Hola");
-
-
     }
+
+
 }
